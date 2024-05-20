33373ddca6ba2b1362dfe8e821c584ec4f1b7631
Roman <pryschepa.roman@gmail.com>
2024-05-20 12:47:01 +0300

    field fixes

--- app.py
+++ app.py
@@ -26,20 +26,21 @@ app = FastAPI(
 
 #parser block
 class candidate(BaseModel): 
-    name: str = Field(description ='Name of the candidate')
-    surname: str = Field(description = 'Surname of the candidate')
+    name: str = Field(description ='Name of the candidate. Start with the uppercase, other - lowercase')
+    surname: str = Field(description = 'Surname of the candidate. Start with the uppercase, other - lowercase')
     gender: str = Field(description = 'Gender of the candidate. Possible values: ["Male", "Female", "Unknown"]')
 
-    dateOfBirth: int = Field(description='Date of Birth of the candidate in unixtime')
+    dateOfBirth: int = Field(description='Date of Birth of the candidate in unixtime. If empty, value equals null.')
     personalEmail: str = Field(description='Personal email of the candidate')
-    phone: str = Field(description='Phone number of the candidate')
-    education: str = Field(description='Education details of the candidate')
-    experience: str = Field(description='Experience details of the candidate')
+    phone: str = Field(description='Phone number of the candidate. Only numbers.')
+    education: str = Field(description='List of Education details of the candidate. Degree, Dates, Study place')
+    experience: str = Field(description='List of experience details of the candidate. Position, Dates, Workplace')
     softSkills: str = Field(description='List of soft skills of the candidate')
     hardSkills: str = Field(description='List of hard skills of the candidate')
 
     score: int = Field(description = 'On score 0-10 how good this candidate is suitable to this vacancy.')
     score_desciption: int = Field(description = 'Why did you decide this candidate is suitable or not.')
+
 
 parser = JsonOutputParser(pydantic_object=candidate)
 #
@@ -47,7 +48,7 @@ parser = JsonOutputParser(pydantic_object=candidate)
 #prompt block
 request = "Help me with this candidate please."
 prompt = PromptTemplate(
-    template ="You are a recruiter in IT Company. I will give you both - vacancy description and a candidate description. Candidate must know/be suitable for the most requierments in the vacancy description, if not - in most cases he is not suitable. You need to answer with a schema.\nVacancy: {vacancy}\nCandidate: {context}\n{format_instructions}",
+    template ="You are a recruiter in IT Company. I will give you both - vacancy description and a candidate description. Candidate must know/be suitable for the most requierments in the vacancy description, if not - in most cases he is not suitable. You need to answer with a json-schema. Do not add comments. \nVacancy: {vacancy}\nCandidate: {context}\n{format_instructions}",
     input_variables =["vacancy","context"],
     partial_variables={"format_instructions": parser.get_format_instructions()},
 )
@@ -57,8 +58,8 @@ def pdfloader(obj):
     print('started pdfloader')
     print(obj)
     if obj['type']=='pdf':
-        #loader = PyPDFLoader(obj['fileUrl'])
-        loader = PyPDFLoader('../OleksandrZima_CV.docx.pdf')
+        loader = PyPDFLoader(obj['fileUrl'])
+        #loader = PyPDFLoader('../OleksandrZima_CV.docx.pdf')
     elif obj['type']=='docx':
         loader = Docx2txtLoader(obj['fileUrl'])
     pages = loader.load()
@@ -75,5 +76,4 @@ def combine_page_contents(pages):
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
